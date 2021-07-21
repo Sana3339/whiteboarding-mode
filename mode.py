4a77785cd70060e95ea62c9b5a81b062b6c166e3
Sana <sanaahmad393@gmail.com>
2021-07-21 18:44:56 +0000

    Initial commit

---
+++ mode.py
@@ -0,0 +1,25 @@
+"""Find the most frequent num(s) in nums.
+
+Return the set of nums that are the mode::
+
+    >>> mode([1]) == {1}
+    True
+
+    >>> mode([1, 2, 2, 2]) == {2}
+    True
+
+If there is a tie, return all::
+
+    >>> mode([1, 1, 2, 2]) == {1, 2}
+    True
+"""
+
+
+def mode(nums):
+    """Find the most frequent num(s) in nums."""
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. HOORAY!\n")
